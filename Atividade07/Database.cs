de94649dd650de43ef7507251e1a64e9706ea75f
BrendomGoncalves <brendom.vi@gmail.com>
2024-05-29 15:51:14 -0300

    feat: Implementando teste 07

--- Atividade07/Database.cs
+++ Atividade07/Database.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Atividade07
+﻿namespace Atividade07
 {
     public class Database : IDatabase
     {
